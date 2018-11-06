d0448e80ccbe6de9ade2f35c2cc695c5e13364bc
Gremmel <meine@soseies.de>
2018-11-06 23:18:28 +0100

    In Entwicklung Flaschenlabel

--- source/src/mainwindowimpl_flaschenlabel.cpp
+++ source/src/mainwindowimpl_flaschenlabel.cpp
@@ -13,17 +13,38 @@ void MainWindowImpl::ErstelleFlaschenlabel()
     QSettings settings(QSettings::IniFormat, QSettings::UserScope, KONFIG_ORDNER, APP_KONFIG);
     QString settingsPath = QFileInfo(settings.fileName()).absolutePath() + "/";
 
-    QFile file(settingsPath + "flaschenlabel.html");
+    QFile file(settingsPath + "streifen_vorlage.svg");
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
         return;
-    QString html_template = file.readAll();
-    file.close();
+    QString svg_template = file.readAll();
 
     Mustache::Renderer renderer;
     Mustache::QtVariantContext context(contextVariables);
-    QString seite = renderer.render(html_template, &context);
-    if (webView_Flaschenlabel->url().isEmpty())
-        MyWebView::clearMemoryCaches();
-    webView_Flaschenlabel->setHtml(seite, QUrl::fromLocalFile(settingsPath));
+    QString svg = renderer.render(svg_template, &context);
+    file.close();
+
+    QFile filenew(settingsPath + "streifen.svg");
+    if (filenew.open(QIODevice::WriteOnly | QIODevice::Text)) {
+      QTextStream out(&filenew);
+      out << svg << endl;
+      filenew.close();
+    }
+
+//    svg_template.remove(0, svg_template.indexOf("<svg"));
+
+    QFile fileHTML(settingsPath + "flaschenlabel.html");
+    if (!fileHTML.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+    QString html_template = fileHTML.readAll();
+    fileHTML.close();
+
+
+
+    QString Seite = renderer.render(html_template, &context);
+//    Seite.replace(QString("##svgLabel##"), svg);
+
+//    if (webView_Flaschenlabel->url().isEmpty())
+    MyWebView::clearMemoryCaches();
+    webView_Flaschenlabel->setHtml(Seite, QUrl::fromLocalFile(settingsPath));
     webView_Flaschenlabel->setLinksExternal(true);
 }
